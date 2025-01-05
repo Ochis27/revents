9d029844c01efd648ce17019cedfad9543cd83e8
Ochis27 <ochispaulcatalin27@gmail.com>
2025-01-05 12:45:50 +0200

    setting application structure

---
+++ src/app/features/events/dashboard/EventDashboard.tsx
@@ -0,0 +1,30 @@
+import {useEffect, useState} from "react";
+import {Grid} from "semantic-ui-react";
+import {sampleData} from "../../../api/sampleData";
+import {AppEvent} from "../../../types/Event";
+import EventForm from "../form/EventForm";
+import EventList from "./EventList";
+
+type Props = {
+  formOpen: boolean;
+  setFormOpen: (value: boolean) => void;
+};
+
+export default function EventDashboard({formOpen, setFormOpen}: Props) {
+  const [events, setEvents] = useState<AppEvent[]>([]);
+
+  useEffect(() => {
+    setEvents(sampleData);
+  }, []);
+
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <EventList events={events} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        {formOpen && <EventForm setFormOpen={setFormOpen} />}
+      </Grid.Column>
+    </Grid>
+  );
+}
--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,7 +1,17 @@
+import {useState} from "react";
+import {Container} from "semantic-ui-react";
+import EventDashboard from "../features/events/dashboard/EventDashboard";
+import NavBar from "./nav/NavBar";
+
 function App() {
+  const [formOpen, setFormOpen] = useState(false);
+
   return (
     <>
-      <h1>Welcome to Revents</h1>
+      <NavBar setFormOpen={setFormOpen} />
+      <Container className="main">
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+      </Container>
     </>
   );
 }
